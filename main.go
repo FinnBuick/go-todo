827e859022be7dd815b593a923ff12d820a4d231
Finn Buick <finnbuick@gmail.com>
2025-05-05 07:28:12 +0200

    feat: commands to add, list, completed todos, loading and saving from json

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"go-todo/todo"
+)
+
+// Commands
+// list - lists the existing todos
+// add "Buy milk" - adds a todo
+// done - marks a todos as completed
 
 func main() {
-	fmt.Println("Hello World!")
+	add := flag.String("add", "", "Task to add")
+	list := flag.Bool("list", false, "List tasks")
+	done := flag.Int("done", -1, "Mark task as done")
+	flag.Parse()
+
+	const file = "todos.json"
+	tasks, err := todo.LoadTasks(file)
+	if err != nil {
+		fmt.Println("Error loading task:", err)
+		return
+	}
+
+	switch {
+	case *add != "":
+		fmt.Println(tasks)
+		t := todo.Task{ID: len(tasks) + 1, Text: *add}
+		tasks = append(tasks, t)
+		todo.SaveTasks(tasks, file)
+		fmt.Println("Added task:", t.Text)
+	case *list:
+		for _, t := range tasks {
+			status := ""
+			if t.Completed {
+				status = "x"
+			}
+			fmt.Printf("[%s] %d: %s\n", status, t.ID, t.Text)
+		}
+	case *done > 0:
+		for i, t := range tasks {
+			if t.ID == *done {
+				tasks[i].Completed = true
+				todo.SaveTasks(tasks, file)
+				fmt.Println("Marked as done:", t.Text)
+				break
+			}
+		}
+	default:
+		fmt.Println("Usage: -add \"task\" | -list | -done N")
+	}
 }
---
+++ todo/store.go
@@ -0,0 +1,27 @@
+package todo
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func SaveTasks(tasks []Task, filename string) error {
+	data, err := json.MarshalIndent(tasks, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, data, 0644)
+}
+
+func LoadTasks(filename string) ([]Task, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return nil, err
+	}
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	return tasks, err
+}
